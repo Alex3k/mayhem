ef9edce9b8198ffbd49af8b5f1affe2372f8a075
Alex Close <adclose93@gmail.com>
2017-01-07 16:04:00 +0000

    Updated file structure so that there isn't a root Mayhem Directory. Also created rudimentary firing logic

---
+++ Assets/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    public class Bullet : MonoBehaviour
+    {
+        public float MovementSpeed = 100;
+        public float TTL = 2;
+        private double m_CreationTime;
+
+        void Start()
+        {
+            m_CreationTime = PhotonNetwork.time;
+        }
+
+        void FixedUpdate()
+        {
+            if(PhotonNetwork.time - m_CreationTime > TTL)
+            {
+                Destroy(transform.gameObject);
+            }
+            else
+            {
+                HandleMovement();
+            }
+        }
+
+        private void HandleMovement()
+        {
+            transform.position += transform.right * MovementSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float MovementSpeed = 3f;
+    public float RotateSpeed = 5f;
+    PhotonView m_PhotonView;
+
+    void Awake()
+    {
+
+        m_PhotonView = GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (m_PhotonView.isMine == false)
+        {
+            return;
+        }
+
+        UpdateMovement();
+        handleWeaponary();
+    }
+
+    void handleWeaponary()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Fire");
+            GameObject player = PhotonNetwork.Instantiate("Bullet", transform.position, Quaternion.Euler(transform.right), 0);
+
+        }
+    }
+
+    void UpdateMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.right * MovementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * RotateSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * -RotateSpeed);
+        }
+    }
+}
