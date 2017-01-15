a3aea31b7dd30575dc4010fd156ec8f8318e84ac
Alex Close <adclose93@gmail.com>
2017-01-15 12:58:18 +0000

    Updated to show user id in game so someone can join their friends game easily

--- Assets/Networking/RoomManager.cs
+++ Assets/Networking/RoomManager.cs
@@ -1,12 +1,14 @@
 ﻿using Mayhem.World;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Mayhem.Networking
 {
-    public class RandomGameMatchMaker : Photon.PunBehaviour
+    public class RoomManager : Photon.PunBehaviour
     {
         public Text ConnectionStatus;
+        public Text RoomNameLabel;
 
         void Awake()
         {
@@ -21,15 +23,21 @@ namespace Mayhem.Networking
         public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
         {
             Debug.Log("Can't join random room! Creating New Room!");
-            PhotonNetwork.CreateRoom(null);
+            PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 10, PublishUserId=true }, null);
+            
         }
 
         public override void OnJoinedRoom()
         {
+            PhotonNetwork.player.UserId = ((DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond) + new System.Random(DateTime.Now.Millisecond).Next(3912)).ToString();
+
             GameObject.Find("LoadingUI").SetActive(false);
+            GameObject.Find("GameUI").SetActive(true);
 
             GameObject player = PhotonNetwork.Instantiate("Sprite", Vector3.zero, Quaternion.identity, 0);
             GameObject.Find("Main Camera").AddComponent<Camera2DFollow>().target = player.transform;
+
+            RoomNameLabel.text = "Your ID: " + PhotonNetwork.player.UserId;
         }
     }
 }
