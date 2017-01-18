edf49dc9e6778b08143aab3702a7b40417141a30
Alex Close <adclose93@gmail.com>
2017-01-18 23:11:17 +0000

    Noticed that flashlight toggling wasn't working over the network. Therefore created FlashLightController to enable it.

---
+++ Assets/Entities/FlashLightController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Mayhem.Entities
+{
+    public class FlashLightController : MonoBehaviour
+    {
+        private Light m_LightSource;
+
+        void Awake()
+        {
+            m_LightSource = GetComponent<Light>();
+        }
+
+        void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.isWriting)
+            {
+                stream.SendNext(m_LightSource.enabled);
+            }
+            else
+            {
+                m_LightSource.enabled = ((bool)stream.ReceiveNext());
+            }
+        }
+    }
+}
