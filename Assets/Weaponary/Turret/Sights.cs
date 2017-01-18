6f3b46b11c14cd6e250118702435d4775c605d64
Alex Close <adclose93@gmail.com>
2017-01-18 20:29:37 +0000

    Created turret that rotates to look at anything that it 'sees'

---
+++ Assets/Weaponary/Turret/Sights.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Mayhem.Weaponary.Turret
+{
+    public class Sights : MonoBehaviour
+    {
+        public bool HasTarget { get; private set; }
+        public Vector3 Target { get; private set; }
+
+        void Awake()
+        {
+            HasTarget = false;
+            Target = Vector2.zero;
+        }
+
+        void OnTriggerStay2D(Collider2D collision)
+        {
+            HasTarget = true;
+            Target = collision.gameObject.transform.position;
+        }
+
+        void OnTriggerExit2D(Collider2D collision)
+        {
+            HasTarget = false;
+            Target = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Weaponary/Turret/Turret.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Mayhem.Weaponary.Turret
+{
+    public class Turret : MonoBehaviour
+    {
+        private Sights m_MySights;
+
+        void Awake()
+        {
+            m_MySights = GetComponentInChildren<Sights>();
+        }
+
+        void Update()
+        {
+            if (m_MySights.HasTarget)
+            {
+                transform.right = m_MySights.Target - transform.position;
+                Debug.Log("Shooting at " + m_MySights.Target);
+            }
+        }
+    }
+}
