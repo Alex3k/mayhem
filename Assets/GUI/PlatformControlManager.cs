6dfd9817df1ae9517dbc17d531601b395c5b170e
Alex Close <adclose93@gmail.com>
2017-01-24 21:11:45 +0000

    Updated so mobile controls only show when the applications runs on a mobile device. Otherwise WASD, Mouse and Tab are used

---
+++ Assets/GUI/PlatformControlManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Mayhem.GUI
+{
+    public class PlatformControlManager : MonoBehaviour
+    {
+        public GameObject MobileControls;
+        
+        void Awake()
+        {
+            if (Application.isMobilePlatform)
+            {
+                MobileControls.SetActive(true);
+            }
+            else
+            {
+                MobileControls.SetActive(false);
+            }
+        }
+    }
+}
