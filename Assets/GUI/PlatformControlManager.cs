fd12020f0f1786d23c50b29a67c7832c61c1f78f
Alex Close <adclose93@gmail.com>
2017-01-24 21:18:14 +0000

    Updated PlatformControlManager so that it handles changing between items and equipment

--- Assets/GUI/PlatformControlManager.cs
+++ Assets/GUI/PlatformControlManager.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Mayhem.GUI
 {
     public class PlatformControlManager : MonoBehaviour
     {
         public GameObject MobileControls;
+        public GameObject EquipmentChanger;
         
         void Awake()
         {
             if (Application.isMobilePlatform)
             {
                 MobileControls.SetActive(true);
+                EquipmentChanger.GetComponent<Button>().enabled = true;
+                EquipmentChanger.GetComponent<Image>().enabled = true;
             }
             else
             {
                 MobileControls.SetActive(false);
+                EquipmentChanger.GetComponent<Button>().enabled = false;
+                EquipmentChanger.GetComponent<Image>().enabled = false;
             }
         }
     }
